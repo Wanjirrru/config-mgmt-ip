2843d8e836816253b7a4eafb0f6556370b5af772
Wanjirrru <melissawanjiru@yahoo.com>
2026-01-11 22:03:26 +0300

    Added variables & configurations

---
+++ stage_two/terraform/main.tf
@@ -0,0 +1,51 @@
+variable "vm_name" {
+  description = "Name of the Vagrant VM"
+  type        = string
+  default     = "yolo-stage2"
+}
+
+variable "box" {
+  description = "Vagrant box to use"
+  type        = string
+  default     = "geerlingguy/ubuntu2004"
+}
+
+variable "memory" {
+  type    = number
+  default = 2048
+}
+
+variable "cpus" {
+  type    = number
+  default = 2
+}
+
+variable "ip_address" {
+  description = "Private IP for the VM"
+  type        = string
+  default     = "192.168.56.20"   # Different from Stage 1 to avoid conflict
+}
+
+# This resource triggers Vagrant up + Ansible deployment
+resource "null_resource" "provision_yolo_stage2" {
+  # Force re-run on every terraform apply (useful for testing)
+  triggers = {
+    vm_destroy = "${timestamp()}"  # Force full re-run
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "===== Starting Stage 2 Deployment ====="
+      cd ../../ && vagrant up --provider=virtualbox
+      echo "Vagrant VM is up. Running Ansible playbook..."
+      vagrant ssh -c "cd /vagrant && ansible-playbook stage_two/ansible/playbook-stage2.yml"
+      echo "Deployment complete! Access app at http://${var.ip_address}:3000"
+    EOT
+  }
+}
+
+# Output the IP for easy reference
+output "vm_ip" {
+  value       = var.ip_address
+  description = "IP address of the Stage 2 VM"
+}
